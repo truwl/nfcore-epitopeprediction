cf47db83ca40298ac23c442fa00962c707a88298
Jeremy Leipzig <leipzig@gmail.com>
2022-03-14 22:29:19 -0600

    bdcli build

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t truwl/nfcore-epitopeprediction:2.0.0_0.1.0 -t truwl/nfcore-epitopeprediction:latest .
+docker push truwl/nfcore-epitopeprediction:2.0.0_0.1.0
+docker push truwl/nfcore-epitopeprediction:latest
+        
